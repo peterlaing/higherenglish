0d89ac984ef62f83207a21b9d3717f0792301512
SigmoidDev <140631558@ea.edin.sch.uk>
2025-02-20 23:38:54 +0000

    Page navigation
    Added a page navigation section as well as areas for themes and character, and also added some text and box shadows

--- navigation/navbar.js
+++ navigation/navbar.js
@@ -1,6 +1,6 @@
 async function importFile()
 {
-    const resp = await fetch("/header/header.html");
+    const resp = await fetch("/navigation/navbar.html");
     const html = await resp.text();
     document.body.insertAdjacentHTML("beforeBegin", html);
 }
